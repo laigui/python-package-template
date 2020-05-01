af9bc109131ce927386dbc134abf72e4d5630215
laigui <laigui@gmail.com>
2020-05-01 20:41:48 +0800

    updated customized content

---
+++ README.rst
@@ -0,0 +1,24 @@
+{{ package_name }}
+==================
+
+{{ package_description }}
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`{{ package_name }}` was written by `Laigui Qin <laigui@gmail.com>`_.
---
+++ setup.py
@@ -0,0 +1,41 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="{{ cookiecutter.package_name }}",
+    version="0.0.1",
+    url="https://github.com/laigui/package_name.git",
+    license='MIT',
+
+    author="Laigui Qin",
+    author_email="laigui@gmail.com",
+
+    description="{{ cookiecutter.package_description }}",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
